5453e35a4554492f783a376318e5da8369cd1592
Amos Jun-yeung Ng <me@amos.ng>
2023-02-25 06:54:01 +1100

    Add e2e test for executing the goodbye demo task

---
+++ tests/test_execution.py
@@ -0,0 +1,18 @@
+import vcr_langchain as vcr
+from langchain.llms import OpenAI
+
+from zamm.agents.employee import ZammEmployee
+
+
+def test_execute_goodbye_task():
+    with open("zamm/resources/tutorials/hello.md") as tutorial:
+        with vcr.use_cassette("tests/resources/goodbye.yaml"):
+            llm = OpenAI(model_name="text-davinci-003", temperature=0, max_tokens=-1)
+            employee = ZammEmployee(llm=llm, terminal_safe_mode=True)
+            results = employee(
+                {
+                    "task": 'Write a script goodbye.sh that prints out "Goodbye world". Execute it.',
+                    "documentation": tutorial.read(),
+                }
+            )
+            assert results is not None
