8ab7d4acffed76b9a5df7e5e5ffce115f69e2ca1
Amos Jun-yeung Ng <me@amos.ng>
2023-02-28 14:03:18 +1100

    Remove unused legacy filesystem tools

--- zamm/agents/tools/__init__.py
+++
@@ -1,6 +0,0 @@
-from langchain.agents.load_tools import _BASE_TOOLS
-
-from .load_tools import _get_files_read, _get_files_write
-
-_BASE_TOOLS["file-reader"] = _get_files_read
-_BASE_TOOLS["file-writer"] = _get_files_write
