d73d3744ec7fd066ab99822965d573555debaf67
Amos Jun-yeung Ng <me@amos.ng>
2023-03-04 16:41:54 +1100

    Add flag for quick toggle of using stop API

--- zamm/llms/chat.py
+++ zamm/llms/chat.py
@@ -6,18 +6,22 @@ from langchain.schema import LLMResult
 from zamm.utils import artificial_stop
 
 CHATGPT_MODEL = "gpt-3.5-turbo"
+ARTIFICIAL_STOPPING = True
 
 
 class ChatWrapper(OpenAIChat):
     def _generate(
         self, prompts: List[str], stop: Optional[List[str]] = None
     ) -> LLMResult:
-        result = super()._generate(prompts)
-        if stop is not None:
-            for generation_list in result.generations:
-                for generation in generation_list:
-                    generation.text = artificial_stop(generation.text, stop)
-        return result
+        if ARTIFICIAL_STOPPING:
+            result = super()._generate(prompts)
+            if stop is not None:
+                for generation_list in result.generations:
+                    for generation in generation_list:
+                        generation.text = artificial_stop(generation.text, stop)
+            return result
+
+        return super()._generate(prompts, stop)
 
 
 def new_openai(model_name: str, max_tokens: int = -1, **kwargs):
