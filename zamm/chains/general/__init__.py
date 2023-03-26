33fd3055e4e7866bf7a145841d658c17c5a8d60e
Amos Jun-yeung Ng <me@amos.ng>
2023-03-27 10:49:42 +1100

    Remove error chain

--- zamm/chains/general/__init__.py
+++ zamm/chains/general/__init__.py
@@ -1,13 +1,11 @@
 from .action_chain import ActionChain
 from .choice.base import ChoicePickerChain
 from .choice.prompt import ChoicePromptTemplate
-from .error import ErrorChain
 from .lax_sequential import LaxSequentialChain
 
 __all__ = [
     "ActionChain",
     "ChoicePickerChain",
     "ChoicePromptTemplate",
-    "ErrorChain",
     "LaxSequentialChain",
 ]
