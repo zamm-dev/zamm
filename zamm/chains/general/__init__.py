0275b683b3e85ec4a1b26acbcb986547b81a5941
Amos Jun-yeung Ng <me@amos.ng>
2023-03-27 10:48:47 +1100

    Remove NoOpChain

--- zamm/chains/general/__init__.py
+++ zamm/chains/general/__init__.py
@@ -3,13 +3,11 @@ from .choice.base import ChoicePickerChain
 from .choice.prompt import ChoicePromptTemplate
 from .error import ErrorChain
 from .lax_sequential import LaxSequentialChain
-from .no_op import NoOpChain
 
 __all__ = [
     "ActionChain",
     "ChoicePickerChain",
     "ChoicePromptTemplate",
-    "NoOpChain",
     "ErrorChain",
     "LaxSequentialChain",
 ]
