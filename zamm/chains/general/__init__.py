b521a280901dedf4d6e7ad06aae5312dfb7266a1
Amos Jun-yeung Ng <me@amos.ng>
2023-03-27 10:07:03 +1100

    Remove ZAMM ChoicePrompt

--- zamm/chains/general/__init__.py
+++ zamm/chains/general/__init__.py
@@ -1,6 +1,6 @@
 from .action_chain import ActionChain
 from .choice.base import ChoiceChain
-from .choice.prompt import ChoicePrompt, ChoicePromptTemplate
+from .choice.prompt import ChoicePromptTemplate
 from .error import ErrorChain
 from .lax_sequential import LaxSequentialChain
 from .no_op import NoOpChain
@@ -8,7 +8,6 @@ from .no_op import NoOpChain
 __all__ = [
     "ActionChain",
     "ChoiceChain",
-    "ChoicePrompt",
     "ChoicePromptTemplate",
     "NoOpChain",
     "ErrorChain",
--- zamm/chains/general/choice/prompt.py
+++ zamm/chains/general/choice/prompt.py
@@ -1,23 +1,11 @@
 from typing import List
 
-from fvalues import F
 from langchain_contrib.prompts import ChainedPromptTemplate
 from langchain_contrib.prompts import (
     ChoicePromptTemplate as ContribChoicePromptTemplate,
 )
 from langchain_contrib.prompts import Templatable
 from langchain_contrib.prompts.choice import list_of_choices
-
-
-class ChoicePrompt(F):
-    prefix_text: str
-    choices: List[str]
-
-    def __new__(cls, f_str: F, prefix_text: str, choices: List[str]):
-        result = super().__new__(cls, f_str, parts=f_str.parts)
-        result.prefix_text = prefix_text
-        result.choices = choices
-        return result
 
 
 def ChoicePromptTemplate(
