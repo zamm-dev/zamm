8e1579e2923665949a2b9e8a70a7ebb7b4496a5a
Amos Jun-yeung Ng <me@amos.ng>
2024-02-29 16:58:52 +1100

    Update clippy script for CI

--- src-tauri/clippy.py
+++ src-tauri/clippy.py
@@ -1,14 +1,16 @@
 import subprocess
 import sys
 
+SEPARATOR = "warning: `async-openai` (lib) generated "
+
 clippy_command = "cargo clippy --color never --manifest-path src-tauri/Cargo.toml --fix --allow-dirty --allow-staged --all-targets --all-features -- -Dwarnings"
 clippy_process = subprocess.Popen(
     clippy_command, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE
 )
 clippy_output = clippy_process.communicate()[0].decode()
 
-checking_zamm_output = clippy_output.split("Checking zamm")[1]
-zamm_output = "\n".join(checking_zamm_output.split("\n")[1:])
+separator_line = clippy_output.split(SEPARATOR)[1]
+zamm_output = "\n".join(separator_line.split("\n")[1:])
 
 print(zamm_output)
 
