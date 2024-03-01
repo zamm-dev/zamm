10a038dd9ea4b3cb5deff8fe32c9900759f1f971
Amos Jun-yeung Ng <me@amos.ng>
2024-03-01 20:16:13 +1100

    Add syntax highlighting to code blocks

---
+++ src-svelte/src/routes/chat/CodeRender.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import Highlight from "svelte-highlight";
+  import bash from "svelte-highlight/languages/bash";
+  import javascript from "svelte-highlight/languages/javascript";
+  import typescript from "svelte-highlight/languages/typescript";
+  import rust from "svelte-highlight/languages/rust";
+  import python from "svelte-highlight/languages/python";
+  import plaintext from "svelte-highlight/languages/plaintext";
+  import "svelte-highlight/styles/github.css";
+
+  export let text: string;
+  export let lang: string;
+
+  function getLanguageStr() {
+    if (lang) {
+      return lang.split(" ")[0];
+    }
+    return "plaintext";
+  }
+
+  function getLanguage() {
+    let languageStr = getLanguageStr();
+    switch (languageStr) {
+      case "sh":
+      case "bash":
+        return bash;
+      case "js":
+      case "javascript":
+        return javascript;
+      case "typescript":
+        return typescript;
+      case "rust":
+        return rust;
+      case "py":
+      case "python":
+        return python;
+      default:
+        return plaintext;
+    }
+  }
+
+  let language = getLanguage();
+</script>
+
+<div class="code">
+  <Highlight {language} code={text} />
+</div>
+
+<style>
+  .code :global(code) {
+    border-radius: var(--corner-roundness);
+    background-color: #ffffff88;
+  }
+</style>
--- src-svelte/src/routes/chat/Message.svelte
+++ src-svelte/src/routes/chat/Message.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { ChatMessage } from "$lib/bindings";
   import MessageUI from "./MessageUI.svelte";
+  import CodeRender from "./CodeRender.svelte";
   import SvelteMarkdown from "svelte-markdown";
 
   export let message: ChatMessage;
@@ -8,7 +9,7 @@
 
 <MessageUI role={message.role} {...$$restProps}>
   <div class="markdown">
-    <SvelteMarkdown source={message.text} />
+    <SvelteMarkdown source={message.text} renderers={{ code: CodeRender }} />
   </div>
 </MessageUI>
 
