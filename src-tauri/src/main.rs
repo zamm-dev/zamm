a211deb81d6f0a1ea53da139c58899ab0291e9c5
Amos Jun-yeung Ng <me@amos.ng>
2024-05-19 16:01:13 +0700

    Manually trigger specta bindings export

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use diesel::sqlite::SqliteConnection;
-
-use setup::api_keys::{setup_api_keys, ApiKeys};
-#[cfg(all(debug_assertions, not(target_os = "windows")))]
-use specta::collect_types;
-
-#[cfg(all(debug_assertions, not(target_os = "windows")))]
-use tauri_specta::ts;
-
-use std::env;
-
-use tokio::sync::Mutex;
+mod cli;
 mod commands;
 mod models;
 #[cfg(test)]
@@ -21,6 +10,18 @@ mod schema;
 mod setup;
 #[cfg(test)]
 mod test_helpers;
+
+use clap::Parser;
+use diesel::sqlite::SqliteConnection;
+use setup::api_keys::{setup_api_keys, ApiKeys};
+#[cfg(debug_assertions)]
+use specta::collect_types;
+use std::env;
+#[cfg(debug_assertions)]
+use tauri_specta::ts;
+use tokio::sync::Mutex;
+
+use cli::{Cli, Commands};
 use commands::{
     chat, get_api_call, get_api_calls, get_api_keys, get_preferences, get_system_info,
     play_sound, set_api_key, set_preferences,
@@ -30,40 +31,48 @@ pub struct ZammDatabase(Mutex<Option<SqliteConnection>>);
 pub struct ZammApiKeys(Mutex<ApiKeys>);
 
 fn main() {
-    #[cfg(all(debug_assertions, not(target_os = "windows")))]
-    ts::export(
-        collect_types![
-            get_api_keys,
-            set_api_key,
-            play_sound,
-            get_preferences,
-            set_preferences,
-            get_system_info,
-            chat,
-            get_api_call,
-            get_api_calls,
-        ],
-        "../src-svelte/src/lib/bindings.ts",
-    )
-    .expect("Failed to export Specta bindings");
+    let cli = Cli::parse();
 
-    let mut possible_db = setup::get_db();
-    let api_keys = setup_api_keys(&mut possible_db);
+    match &cli.command {
+        #[cfg(debug_assertions)]
+        Some(Commands::ExportBindings {}) => {
+            ts::export(
+                collect_types![
+                    get_api_keys,
+                    set_api_key,
+                    play_sound,
+                    get_preferences,
+                    set_preferences,
+                    get_system_info,
+                    chat,
+                    get_api_call,
+                    get_api_calls,
+                ],
+                "../src-svelte/src/lib/bindings.ts",
+            )
+            .expect("Failed to export Specta bindings");
+            println!("Specta bindings should be exported to ../src-svelte/src/lib/bindings.ts");
+        }
+        Some(Commands::Gui {}) | None => {
+            let mut possible_db = setup::get_db();
+            let api_keys = setup_api_keys(&mut possible_db);
 
-    tauri::Builder::default()
-        .manage(ZammDatabase(Mutex::new(possible_db)))
-        .manage(ZammApiKeys(Mutex::new(api_keys)))
-        .invoke_handler(tauri::generate_handler![
-            get_api_keys,
-            set_api_key,
-            play_sound,
-            get_preferences,
-            set_preferences,
-            get_system_info,
-            chat,
-            get_api_call,
-            get_api_calls,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+            tauri::Builder::default()
+                .manage(ZammDatabase(Mutex::new(possible_db)))
+                .manage(ZammApiKeys(Mutex::new(api_keys)))
+                .invoke_handler(tauri::generate_handler![
+                    get_api_keys,
+                    set_api_key,
+                    play_sound,
+                    get_preferences,
+                    set_preferences,
+                    get_system_info,
+                    chat,
+                    get_api_call,
+                    get_api_calls,
+                ])
+                .run(tauri::generate_context!())
+                .expect("error while running tauri application");
+        }
+    }
 }
