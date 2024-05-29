31b706ed925d16824e55d43b1c965a51c52619d5
Amos Jun-yeung Ng <me@amos.ng>
2024-05-29 14:21:42 +0700

    Make API call link retrieval more deterministic

---
+++ src-tauri/migrations/2024-05-29-071520_ordered_names/down.sql
@@ -0,0 +1,11 @@
+DROP VIEW llm_call_named_follow_ups;
+CREATE VIEW llm_call_named_follow_ups AS
+  SELECT
+    llm_call_follow_ups.previous_call_id AS previous_call_id,
+    previous_call.completion AS previous_call_completion,
+    llm_call_follow_ups.next_call_id AS next_call_id,
+    next_call.completion AS next_call_completion
+  FROM
+    llm_call_follow_ups
+    JOIN llm_calls AS previous_call ON llm_call_follow_ups.previous_call_id = previous_call.id
+    JOIN llm_calls AS next_call ON llm_call_follow_ups.next_call_id = next_call.id;
