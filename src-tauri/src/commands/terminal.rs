0c5060479d6efd53d8ba8df6630048116459166a
Amos Jun-yeung Ng <me@amos.ng>
2024-07-10 21:45:46 +0700

    Use shlex to simplify command processing

--- src-tauri/src/commands/terminal.rs
+++ src-tauri/src/commands/terminal.rs
@@ -1,26 +1,12 @@
 use super::errors::ZammResult;
 use crate::models::asciicasts::AsciiCastData;
+use anyhow::anyhow;
 use async_trait::async_trait;
 use duct::cmd;
 
-pub fn command_args_to_string(command: &str, args: &[&str]) -> String {
-    let escaped_args = args
-        .iter()
-        .map(|arg| {
-            if arg.contains(' ') {
-                format!("\"{}\"", arg)
-            } else {
-                arg.to_string()
-            }
-        })
-        .collect::<Vec<String>>();
-    format!("{} {}", command, escaped_args.join(" "))
-}
-
 #[async_trait]
 pub trait Terminal {
-    async fn run_command(&mut self, command: &str, args: &[&str])
-        -> ZammResult<String>;
+    async fn run_command(&mut self, command: &str) -> ZammResult<String>;
 }
 
 pub struct ActualTerminal {
@@ -38,17 +24,19 @@ impl ActualTerminal {
 
 #[async_trait]
 impl Terminal for ActualTerminal {
-    async fn run_command(
-        &mut self,
-        command: &str,
-        args: &[&str],
-    ) -> ZammResult<String> {
-        self.session_data.header.command = Some(command_args_to_string(command, args));
+    async fn run_command(&mut self, command: &str) -> ZammResult<String> {
+        let cmd_and_args = shlex::split(command)
+            .ok_or_else(|| anyhow!("Failed to split command '{}'", command))?;
+        self.session_data.header.command = Some(command.to_string());
 
         let starting_time = chrono::Utc::now();
         self.session_data.header.timestamp = Some(starting_time);
 
-        let output = cmd(command, args).stderr_to_stdout().read()?;
+        let parsed_cmd = cmd_and_args
+            .first()
+            .ok_or_else(|| anyhow!("Failed to get command"))?;
+        let parsed_args = &cmd_and_args[1..];
+        let output = cmd(parsed_cmd, parsed_args).stderr_to_stdout().read()?;
         let output_time = chrono::Utc::now();
         let duration = output_time - starting_time;
         self.session_data.entries.push(asciicast::Entry {
@@ -69,14 +57,11 @@ mod tests {
         let mut terminal = ActualTerminal::new();
         #[cfg(target_os = "windows")]
         let output = terminal
-            .run_command("cmd", &["/C", "echo hello world"])
+            .run_command("cmd /C \"echo hello world\"")
             .await
             .unwrap();
         #[cfg(not(target_os = "windows"))]
-        let output = terminal
-            .run_command("echo", &["hello", "world"])
-            .await
-            .unwrap();
+        let output = terminal.run_command("echo hello world").await.unwrap();
         assert_eq!(output, "hello world");
     }
 
@@ -84,7 +69,7 @@ mod tests {
     async fn test_capture_interleaved_output() {
         let mut terminal = ActualTerminal::new();
         let output = terminal
-            .run_command("python", &["api/sample-terminal-sessions/interleaved.py"])
+            .run_command("python api/sample-terminal-sessions/interleaved.py")
             .await
             .unwrap();
 
