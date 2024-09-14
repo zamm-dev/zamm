3b3de75b8a1bfbe9376ceac8dbbbaae63593c815
Amos Jun-yeung Ng <me@amos.ng>
2024-09-15 00:48:24 +0700

    Allow for different separator cutoffs

--- src-tauri/clippy.py
+++ src-tauri/clippy.py
@@ -1,7 +1,13 @@
 import subprocess
 import sys
 
-SEPARATOR = "warning: `ollama-rs` (lib) generated "
+
+def cut_off_separate(output: str, separator: str) -> str:
+    if separator not in output:
+        return output
+    separator_line = output.split(separator)[1]
+    return "\n".join(separator_line.split("\n")[1:])
+
 
 clippy_command = "cargo clippy --color never --manifest-path src-tauri/Cargo.toml --fix --allow-dirty --allow-staged --all-targets --all-features -- -Dwarnings"
 clippy_process = subprocess.Popen(
@@ -9,11 +15,10 @@ clippy_process = subprocess.Popen(
 )
 clippy_output = clippy_process.communicate()[0].decode()
 
-if SEPARATOR not in clippy_output:
-    zamm_output = clippy_output
-else:
-    separator_line = clippy_output.split(SEPARATOR)[1]
-    zamm_output = "\n".join(separator_line.split("\n")[1:])
+zamm_output = cut_off_separate(
+    clippy_output, "warning: `async-openai` (lib) generated "
+)
+zamm_output = cut_off_separate(zamm_output, "warning: `ollama-rs` (lib) generated ")
 
 print(zamm_output)
 
