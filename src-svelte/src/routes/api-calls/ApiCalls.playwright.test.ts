6983b3683a2195b0584cbc9847f140e8c69ddc08
Amos Jun-yeung Ng <me@amos.ng>
2024-09-15 13:14:14 +0700

    Try to ensure increased timeout of ApiCalls test to avoid flakiness on CI

--- src-svelte/src/routes/api-calls/ApiCalls.playwright.test.ts
+++ src-svelte/src/routes/api-calls/ApiCalls.playwright.test.ts
@@ -30,6 +30,7 @@ describe("Api Calls endless scroll test", () => {
 
   beforeEach(async () => {
     page = await context.newPage();
+    page.setDefaultTimeout(PLAYWRIGHT_TIMEOUT);
     if (DEBUG_LOGGING) {
       page.on("console", (msg) => {
         console.log(msg);
@@ -63,7 +64,9 @@ describe("Api Calls endless scroll test", () => {
     const lastMessageContainer = apiCallsScrollElement.locator(
       "a:nth-last-child(2) .message.instance .text-container",
     );
-    await expect(lastMessageContainer).toHaveText(expectedValue);
+    await expect(lastMessageContainer).toHaveText(expectedValue, {
+      timeout: PLAYWRIGHT_TIMEOUT,
+    });
   };
 
   test(
