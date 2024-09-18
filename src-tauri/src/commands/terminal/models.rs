38cb7e18118cb8932b60628df85aaab5eab53e81
Amos Jun-yeung Ng <me@amos.ng>
2024-09-18 17:20:02 +0700

    Get non-exiting process capture working on Mac OS

--- src-tauri/src/commands/terminal/models.rs
+++ src-tauri/src/commands/terminal/models.rs
@@ -1,7 +1,12 @@
 use crate::commands::errors::ZammResult;
 use crate::models::asciicasts::AsciiCastData;
+use anyhow::anyhow;
 use async_trait::async_trait;
+use rexpect::session::PtySession;
 use rexpect::spawn;
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::Duration;
 
 #[async_trait]
 pub trait Terminal: Send + Sync {
@@ -10,35 +15,73 @@ pub trait Terminal: Send + Sync {
 }
 
 pub struct ActualTerminal {
+    pub session: Option<Arc<Mutex<PtySession>>>,
     pub session_data: AsciiCastData,
 }
 
 impl ActualTerminal {
     pub fn new() -> Self {
         Self {
+            session: None,
             session_data: AsciiCastData::new(),
         }
     }
-}
 
-#[async_trait]
-impl Terminal for ActualTerminal {
-    async fn run_command(&mut self, command: &str) -> ZammResult<String> {
-        self.session_data.header.command = Some(command.to_string());
+    fn drain_read_buffer(&mut self) -> ZammResult<String> {
+        let mut output = String::new();
+        if let Some(session) = self.session.as_mut() {
+            let reader = &mut session.lock()?.reader;
+            while let Some(chunk) = reader.try_read() {
+                output.push(chunk);
+            }
+        }
+        Ok(output)
+    }
 
-        let starting_time = chrono::Utc::now();
-        self.session_data.header.timestamp = Some(starting_time);
+    fn read_updates(&mut self) -> ZammResult<String> {
+        let mut output = String::new();
+        loop {
+            sleep(Duration::from_millis(100));
+            let new_output = self.drain_read_buffer()?;
+            if new_output.is_empty() {
+                break;
+            }
+            output.push_str(&new_output);
+        }
 
-        let mut session = spawn(command, None)?;
-        let output = session.exp_eof()?;
         let output_time = chrono::Utc::now();
-        let duration = output_time - starting_time;
+        let duration = output_time
+            - self
+                .session_data
+                .header
+                .timestamp
+                .ok_or(anyhow!("No timestamp"))?;
         self.session_data.entries.push(asciicast::Entry {
             time: duration.num_milliseconds() as f64 / 1000.0,
             event_type: asciicast::EventType::Output,
             event_data: output.clone(),
         });
         Ok(output)
+    }
+}
+
+#[async_trait]
+impl Terminal for ActualTerminal {
+    async fn run_command(&mut self, command: &str) -> ZammResult<String> {
+        if self.session.is_some() {
+            return Err(anyhow!("Session already started").into());
+        }
+
+        self.session_data.header.command = Some(command.to_string());
+
+        let starting_time = chrono::Utc::now();
+        self.session_data.header.timestamp = Some(starting_time);
+
+        let session = spawn(command, Some(1_000))?;
+        self.session = Some(Arc::new(Mutex::new(session)));
+
+        let result = self.read_updates()?;
+        Ok(result)
     }
 
     fn get_cast(&self) -> &AsciiCastData {
@@ -72,5 +115,13 @@ mod tests {
             .unwrap();
 
         assert_eq!(output, "stdout\r\nstderr\r\nstdout\r\n");
+    }
+
+    #[tokio::test]
+    async fn test_capture_output_without_blocking() {
+        let mut terminal = ActualTerminal::new();
+        let output = terminal.run_command("bash").await.unwrap();
+
+        assert!(output.ends_with("bash-3.2$ "), "Output: {}", output);
     }
 }
