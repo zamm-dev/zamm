d5c8c09510b38556bfd0e881a7bdb9053ff1ee87
Amos Jun-yeung Ng <me@amos.ng>
2024-10-10 20:29:02 +0700

    Add link to new terminal session page

--- src-svelte/src/routes/api-calls/ApiCalls.svelte
+++ src-svelte/src/routes/api-calls/ApiCalls.svelte
@@ -1,18 +1,44 @@
+<script lang="ts" context="module">
+  import { writable } from "svelte/store";
+
+  type DataTypeEnum = "llm-calls" | "terminal";
+  export const dataType = writable<DataTypeEnum>("llm-calls");
+</script>
+
 <script lang="ts">
   import InfoBox from "$lib/InfoBox.svelte";
   import IconAdd from "~icons/mingcute/add-fill";
   import ApiCallsTable from "./ApiCallsTable.svelte";
+  import Select from "$lib/controls/Select.svelte";
+  import EmptyPlaceholder from "$lib/EmptyPlaceholder.svelte";
 
   export let dateTimeLocale: string | undefined = undefined;
   export let timeZone: string | undefined = undefined;
+
+  $: infoBoxTitle =
+    $dataType === "llm-calls" ? "LLM API Calls" : "Terminal Sessions";
+  $: newHref = $dataType === "llm-calls" ? "/api-calls/new/" : "/terminal/new/";
+  $: newTitle =
+    $dataType === "llm-calls" ? "New API call" : "New Terminal Session";
 </script>
 
-<InfoBox title="LLM API Calls" fullHeight>
+<InfoBox title={infoBoxTitle} fullHeight>
   <div class="container full-height">
-    <a class="new-api-call" href="/api-calls/new/" title="New API call">
+    <a class="new-button" href={newHref} title={newTitle}>
       <IconAdd />
     </a>
-    <ApiCallsTable {dateTimeLocale} {timeZone} />
+    <Select name="data-type" label="Showing " bind:value={$dataType}>
+      <option value="llm-calls">LLM Calls</option>
+      <option value="terminal">Terminal</option>
+    </Select>
+    {#if $dataType === "llm-calls"}
+      <ApiCallsTable {dateTimeLocale} {timeZone} />
+    {:else}
+      <EmptyPlaceholder>
+        Terminal sessions cannot be viewed yet.<br />You may
+        <a href="/terminal/new/">start</a> a new one.
+      </EmptyPlaceholder>
+    {/if}
   </div>
 </InfoBox>
 
@@ -21,13 +47,21 @@
     gap: 0.25rem;
   }
 
-  a.new-api-call {
+  .container :global(.select-wrapper) {
+    flex: 0;
+  }
+
+  .container :global(select) {
+    width: fit-content;
+  }
+
+  a.new-button {
     position: absolute;
     top: 1rem;
     right: 1rem;
   }
 
-  a.new-api-call :global(svg) {
+  a.new-button :global(svg) {
     transform: scale(1.2);
     color: var(--color-faded);
   }
---
+++ src-svelte/src/routes/terminal/new/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import TerminalSession from "../TerminalSession.svelte";
+</script>
+
+<TerminalSession />
