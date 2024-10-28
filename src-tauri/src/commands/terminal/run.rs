f58dad8be6b3dec472dfd3d9fbc3b89bebc7f286
Amos Jun-yeung Ng <me@amos.ng>
2024-10-28 15:48:14 +0700

    Clean terminal run output before returning back to frontend for display to user

--- src-tauri/src/commands/terminal/run.rs
+++ src-tauri/src/commands/terminal/run.rs
@@ -12,6 +12,17 @@ use serde::{Deserialize, Serialize};
 use specta::specta;
 use tauri::State;
 
+#[derive(PartialEq)]
+enum EscapeSequence {
+    None,
+    Start,
+    InEscape,
+    InOperatingSystemEscape,
+    LineStart,
+}
+
+static ESCAPE_COMMANDS: &[char] = &['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'b', 'm'];
+
 #[derive(Debug, Clone, Serialize, Deserialize, specta::Type)]
 pub struct RunCommandResponse {
     pub id: EntityId,
@@ -20,9 +31,83 @@ pub struct RunCommandResponse {
 }
 
 fn clean_output(output: &str) -> String {
-    strip_ansi_escapes::strip_str(output)
-        .trim_start()
-        .to_string()
+    let mut cleaned_lines = Vec::<String>::new();
+    output.split('\n').for_each(|line| {
+        let mut escape: EscapeSequence = EscapeSequence::None;
+        let mut cleaned_line = String::new();
+        let mut current_escape_arg = String::new();
+        let mut escape_args = Vec::<String>::new();
+        let mut escape_command = ' ';
+        line.chars().for_each(|c| {
+            if c == '\u{001B}' {
+                escape = EscapeSequence::Start;
+            } else if escape == EscapeSequence::Start {
+                if c == '[' || c == '(' {
+                    escape = EscapeSequence::InEscape;
+                } else if c == ']' {
+                    escape = EscapeSequence::InOperatingSystemEscape;
+                } else {
+                    escape = EscapeSequence::None;
+                    cleaned_line.push(c);
+                }
+            } else if escape == EscapeSequence::InEscape
+                || escape == EscapeSequence::InOperatingSystemEscape
+            {
+                if c == '?' {
+                    // it's just a private sequence marker, do nothing
+                } else if escape == EscapeSequence::InEscape
+                    && ESCAPE_COMMANDS.contains(&c)
+                {
+                    escape_command = c;
+                    escape_args.push(current_escape_arg.clone());
+                    current_escape_arg.clear();
+                    escape = EscapeSequence::None;
+                } else if c == ';' {
+                    escape_args.push(current_escape_arg.clone());
+                    current_escape_arg.clear();
+                } else if c == '\u{0007}' {
+                    if let Some(last_char) = current_escape_arg.pop() {
+                        escape_command = last_char;
+                    }
+                    escape_args.push(current_escape_arg.clone());
+                    current_escape_arg.clear();
+                    escape = EscapeSequence::None;
+                } else {
+                    current_escape_arg.push(c);
+                }
+
+                if escape == EscapeSequence::None {
+                    escape_args.push(current_escape_arg.clone());
+                    current_escape_arg.clear();
+
+                    // now we actually handle the escape sequence
+                    if escape_command == 'H' {
+                        if let Some(first_arg) = escape_args.first() {
+                            if let Ok(row) = first_arg.parse::<usize>() {
+                                if row > cleaned_lines.len() {
+                                    cleaned_lines.push(cleaned_line.clone());
+                                    cleaned_line.clear();
+                                    // -1 because the new cleaned_line will be added at
+                                    // the end as the next line
+                                    cleaned_lines.resize(row - 1, "".to_string());
+                                }
+                            }
+                        }
+                    }
+                }
+            } else if c == '\r' {
+                escape = EscapeSequence::LineStart;
+            } else if escape == EscapeSequence::LineStart {
+                escape = EscapeSequence::None;
+                cleaned_line.clear();
+                cleaned_line.push(c);
+            } else {
+                cleaned_line.push(c);
+            }
+        });
+        cleaned_lines.push(cleaned_line);
+    });
+    cleaned_lines.join("\n").trim_start().to_string()
 }
 
 async fn run_command_helper(
@@ -92,7 +177,6 @@ pub async fn run_command(
 #[cfg(test)]
 mod tests {
     use super::*;
-
     use crate::check_sample;
     use crate::sample_call::SampleCall;
     use crate::test_helpers::api_testing::standard_test_subdir;
