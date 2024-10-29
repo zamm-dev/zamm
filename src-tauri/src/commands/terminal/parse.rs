aa08380fb8e2e216c4ae24b1e0b65f4f4aeeaf68
Amos Jun-yeung Ng <me@amos.ng>
2024-10-29 10:11:32 +0700

    Rename output parser fields

--- src-tauri/src/commands/terminal/parse.rs
+++ src-tauri/src/commands/terminal/parse.rs
@@ -10,11 +10,11 @@ pub enum EscapeSequence {
 static ESCAPE_COMMANDS: &[char] = &['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'b', 'm'];
 
 pub struct OutputParser {
+    pub state: EscapeSequence,
     pub cleaned_lines: Vec<String>,
-    pub cleaned_line: String,
-    pub escape: EscapeSequence,
-    pub current_escape_arg: String,
+    pub current_line: String,
     pub escape_args: Vec<String>,
+    pub current_arg: String,
 }
 
 impl OutputParser {
@@ -27,8 +27,8 @@ impl OutputParser {
             if let Some(first_arg) = self.escape_args.first() {
                 if let Ok(row) = first_arg.parse::<usize>() {
                     if row > self.cleaned_lines.len() {
-                        self.cleaned_lines.push(self.cleaned_line.clone());
-                        self.cleaned_line.clear();
+                        self.cleaned_lines.push(self.current_line.clone());
+                        self.current_line.clear();
                         // -1 because the new cleaned_line will be added at
                         // the end as the next line
                         self.cleaned_lines.resize(row - 1, "".to_string());
@@ -42,11 +42,11 @@ impl OutputParser {
 impl Default for OutputParser {
     fn default() -> Self {
         OutputParser {
+            state: EscapeSequence::None,
             cleaned_lines: Vec::<String>::new(),
-            cleaned_line: String::new(),
-            escape: EscapeSequence::None,
-            current_escape_arg: String::new(),
+            current_line: String::new(),
             escape_args: Vec::<String>::new(),
+            current_arg: String::new(),
         }
     }
 }
@@ -56,41 +56,41 @@ pub fn clean_output(output: &str) -> String {
 
     output.chars().for_each(|c| {
         if c == '\u{001B}' {
-            parser.escape = EscapeSequence::Start;
-        } else if parser.escape == EscapeSequence::Start {
+            parser.state = EscapeSequence::Start;
+        } else if parser.state == EscapeSequence::Start {
             if c == '[' || c == '(' {
-                parser.escape = EscapeSequence::InEscape;
+                parser.state = EscapeSequence::InEscape;
             } else if c == ']' {
-                parser.escape = EscapeSequence::InOperatingSystemEscape;
+                parser.state = EscapeSequence::InOperatingSystemEscape;
             } else {
-                parser.escape = EscapeSequence::None;
-                parser.cleaned_line.push(c);
+                parser.state = EscapeSequence::None;
+                parser.current_line.push(c);
             }
-        } else if parser.escape == EscapeSequence::InEscape
-            || parser.escape == EscapeSequence::InOperatingSystemEscape
+        } else if parser.state == EscapeSequence::InEscape
+            || parser.state == EscapeSequence::InOperatingSystemEscape
         {
             let mut escape_command: Option<char> = None;
             if c == '?' {
                 // it's just a private sequence marker, do nothing
-            } else if parser.escape == EscapeSequence::InEscape
+            } else if parser.state == EscapeSequence::InEscape
                 && ESCAPE_COMMANDS.contains(&c)
             {
                 escape_command = Some(c);
-                parser.escape = EscapeSequence::None;
+                parser.state = EscapeSequence::None;
             } else if c == ';' {
-                parser.escape_args.push(parser.current_escape_arg.clone());
-                parser.current_escape_arg.clear();
+                parser.escape_args.push(parser.current_arg.clone());
+                parser.current_arg.clear();
             } else if c == '\u{0007}' {
-                escape_command = parser.current_escape_arg.pop();
-                parser.escape_args.push(parser.current_escape_arg.clone());
-                parser.escape = EscapeSequence::None;
+                escape_command = parser.current_arg.pop();
+                parser.escape_args.push(parser.current_arg.clone());
+                parser.state = EscapeSequence::None;
             } else {
-                parser.current_escape_arg.push(c);
+                parser.current_arg.push(c);
             }
 
-            if parser.escape == EscapeSequence::None {
-                parser.escape_args.push(parser.current_escape_arg.clone());
-                parser.current_escape_arg.clear();
+            if parser.state == EscapeSequence::None {
+                parser.escape_args.push(parser.current_arg.clone());
+                parser.current_arg.clear();
 
                 if let Some(escape_command) = escape_command {
                     parser.handle_escape_command(escape_command);
@@ -99,26 +99,26 @@ pub fn clean_output(output: &str) -> String {
                 parser.escape_args.clear();
             }
         } else if c == '\r' {
-            parser.escape = EscapeSequence::LineStart;
-        } else if parser.escape == EscapeSequence::LineStart {
+            parser.state = EscapeSequence::LineStart;
+        } else if parser.state == EscapeSequence::LineStart {
             if c == '\n' {
-                parser.escape = EscapeSequence::None;
-                parser.cleaned_lines.push(parser.cleaned_line.clone());
-                parser.cleaned_line.clear();
+                parser.state = EscapeSequence::None;
+                parser.cleaned_lines.push(parser.current_line.clone());
+                parser.current_line.clear();
             } else {
-                parser.escape = EscapeSequence::None;
-                parser.cleaned_line.clear();
-                parser.cleaned_line.push(c);
+                parser.state = EscapeSequence::None;
+                parser.current_line.clear();
+                parser.current_line.push(c);
             }
         } else if c == '\n' {
-            parser.escape = EscapeSequence::None;
-            parser.cleaned_lines.push(parser.cleaned_line.clone());
-            parser.cleaned_line.clear();
+            parser.state = EscapeSequence::None;
+            parser.cleaned_lines.push(parser.current_line.clone());
+            parser.current_line.clear();
         } else {
-            parser.cleaned_line.push(c);
+            parser.current_line.push(c);
         }
     });
 
-    parser.cleaned_lines.push(parser.cleaned_line);
+    parser.cleaned_lines.push(parser.current_line);
     parser.cleaned_lines.join("\n").trim_start().to_string()
 }
